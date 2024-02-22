fc3e2b15c7df86d83779efb272b4391551d66bc3
Milton Bolonha <miltonbolonha@gmail.com>
2024-02-22 17:30:41 -0300

    update repo

--- next-boilerplate/src/pages/_document.js
+++ next-boilerplate/src/pages/_document.js
@@ -37,7 +37,10 @@ export default class MyDocument extends Document {
             type='text/css'
             href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css'
           />
-          <script async src='https://rampjs-cdn.system1.com/ramp.js'></script>
+          <Script
+            strategy='afterInteractive'
+            src='https://rampjs-cdn.system1.com/ramp.js'
+          />
           <link rel='icon prefetch' href='/favicon.png' sizes='any' />
         </Head>
         <body className='theme-one'>
@@ -45,8 +48,6 @@ export default class MyDocument extends Document {
             strategy='afterInteractive'
             src={`https://www.googletagmanager.com/gtag/js?id=${mainInfos.business.gaID}`}
           />
-          <div id='rampjs_slot1'></div>
-          <div id='rampjs_slot2'></div>
           <Main />
           <NextScript />
         </body>
