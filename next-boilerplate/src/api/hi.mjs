d539f5557d5838d0b318643378f9d5b5294a2258
Milton Bolonha <miltonbolonha@gmail.com>
2024-02-26 01:51:19 -0300

    update repo

--- next-boilerplate/src/api/hi.mjs
+++ next-boilerplate/src/api/hi.mjs
@@ -1,6 +1,6 @@
 export const handler = async (event, context) => {
   return {
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: "Hello World", contextodo: context }),
     statusCode: 200,
   };
 };
