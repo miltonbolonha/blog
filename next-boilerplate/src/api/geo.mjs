52de461c0524055aea313ea0cc117ee7435124d7
Milton Bolonha <miltonbolonha@gmail.com>
2024-02-26 01:47:55 -0300

    update repo

--- next-boilerplate/src/api/geo.mjs
+++ next-boilerplate/src/api/geo.mjs
@@ -1,6 +1,28 @@
-export const handler = async (event, context) => {
+export const handler = async (request, context) => {
   return {
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ geo: context.geo }),
     statusCode: 200,
   };
 };
+
+// export default async (request: Request, context: Context) => {
+//   // Here's what's available on context.geo
+
+//   // context: {
+//   //   geo: {
+//   //     city?: string;
+//   //     country?: {
+//   //       code?: string;
+//   //       name?: string;
+//   //     },
+//   //     subdivision?: {
+//   //       code?: string;
+//   //       name?: string;
+//   //     },
+//   //     latitude?: number;
+//   //     longitude?: number;
+//   //     timezone?: string;
+//   //   }
+//   // }
+
+// };
