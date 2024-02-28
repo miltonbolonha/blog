3bf9ef5acebcbd64efb089170350a97ec932fe43
Milton Bolonha <miltonbolonha@gmail.com>
2024-02-28 17:18:10 -0300

    update repo

--- next-boilerplate/src/pages/_document.js
+++ next-boilerplate/src/pages/_document.js
@@ -54,15 +54,15 @@ export default class MyDocument extends Document {
             async
             dangerouslySetInnerHTML={{
               __html: `
-          if (window.netlifyIdentity) {
-            window.netlifyIdentity.on("init", user => {
-              if (!user) {
-                window.netlifyIdentity.on("login", () => {
-                  document.location.href = "/admin/";
+              if (window.netlifyIdentity) {
+                window.netlifyIdentity.on("init", (user) => {
+                  if (!user) {
+                    window.netlifyIdentity.on("login", () => {
+                      document.location.href = "/admin/";
+                    });
+                  }
                 });
               }
-            });
-          }
         `,
             }}
           />
