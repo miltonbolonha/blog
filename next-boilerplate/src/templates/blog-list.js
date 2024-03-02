d02a282a749b4842bbe2b26e1ffdd41069ae0e19
Milton Bolonha <miltonbolonha@gmail.com>
2024-03-02 07:45:54 -0300

    feature/library page added

--- next-boilerplate/src/templates/blog-list.js
+++ next-boilerplate/src/templates/blog-list.js
@@ -19,6 +19,9 @@ const BlogList = ({ posts, postsToShow, city }) => {
         if (i >= postsToShow) {
           return null;
         }
+        if (post?.frontmatter?.categories[0] === "Admin") {
+          return null;
+        }
         const x = i + 1;
         return (
           <Post
